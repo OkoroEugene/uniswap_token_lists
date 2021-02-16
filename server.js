e1fae1e36c5d8e6462261b7841f9e0a59a8e8fc9
OkoroEugene <obinna.okoroeugene@gmail.com>
2021-02-16 10:59:44 +0100

    refactor routes

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const app = require("express")();
+var cors = require('cors')
+app.use(cors())
 const token_lists = require("./build/uniswap-default.tokenlist.json");
 
 app.get("/", (req, res) => {
     res.json("Welcome to uniswap token listing");
 })
 
-app.get("/list-tokens", (req, res) => {
+app.get("/token/list/single", (req, res) => {
+    res.json(token_lists.tokens[0])
+})
+
+app.get("/token/lists", (req, res) => {
     res.json(token_lists)
 })
 
